5f155ffc1010e236f1dd52dcf422f0b8bb39bd91
Akeel Lashley <akeel@futuretek.ca>
2024-03-08 11:00:13 -0500

    add mui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import ProductCard from "../components/ProductCard";
+import image1 from "../assets/image1.jpg";
+import products from "../data/products.json";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const productsArray = products.products.data.items;
+  return (
+    <>
+      {productsArray.map((product) => (
+        <ProductCard
+          image={image1}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          rating={4.5}
+        />
+      ))}
+
+      {/* <ProductCard
+        image={image1}
+        name="John Doe"
+        description="This is a description."
+        price={99.99}
+        rating={4.5}
+      /> */}
+    </>
+  );
 };
 
 export default Home;
